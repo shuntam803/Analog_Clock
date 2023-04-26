3d58b26a60022f715522585c61e457659f461821
shuntam803 <hopstepwgn0126@gmail.com>
2023-04-26 21:33:54 +0900

    clean: オーディオ関数と変数のリファクタリング

--- js/sound.js
+++ js/sound.js
@@ -1,45 +1,44 @@
-var muteflag = true;
-var defaultaudio = new Audio("sound/menso-re.mp3"); // 毎時0分の音源を指定
-var alarmaudio = new Audio("sound/cuckoo.mp3"); //アラーム音源の指定
-alarmaudio.loop = true;
-defaultaudio.muted = true;
-alarmaudio.muted = true;
+let isMuted = true;
+const hourlySound = new Audio("sound/menso-re.mp3"); // 毎時0分の音源を指定
+const alarmSound = new Audio("sound/cuckoo.mp3"); //アラーム音源の指定
+alarmSound.loop = true;
+hourlySound.muted = true;
+alarmSound.muted = true;
 
-
-function PlayAudio() {
-    console.log("再生")
-    defaultaudio.play();
+function playHourlySound() {
+  console.log("毎時0分の音源再生");
+  hourlySound.play();
 }
 
-function StopAudio() {
-    console.log("停止")
-    defaultaudio.pause();
-    defaultaudio.currentTime = 0;
+function stopHourlySound() {
+  console.log("毎時0分の音源停止");
+  hourlySound.pause();
+  hourlySound.currentTime = 0;
 }
 
-function AlarmStart(){
-    console.log("警告音")
-    alarmaudio.play();
-    alarmcolor();
+function playAlarmSound() {
+  console.log("アラーム音源再生");
+  alarmSound.play();
+  alarmColor();
 }
 
-function Alarmstop(){
-    alarmaudio.pause();
-    defaultcolor();
-    alarmaudio.currentTime = 0;
+function stopAlarmSound() {
+  console.log("アラーム音源再生");
+  alarmSound.pause();
+  defaultColor();
+  alarmSound.currentTime = 0;
 }
 
-function MuteAudio() {
-    
-    if(muteflag){
-        console.log("ミュート解除")
-        muteflag = false
-        defaultaudio.muted = false;
-        alarmaudio.muted = false;
-    }else{
-        console.log("ミュート")
-        muteflag = true
-        defaultaudio.muted = true;
-        alarmaudio.muted = true;
-    }
+function clickMute() {
+  const onIcon = document.querySelector(".on-icon");
+  const offIcon = document.querySelector(".off-icon");
+
+  isMuted = !isMuted;
+  hourlySound.muted = isMuted;
+  alarmSound.muted = isMuted;
+
+  onIcon.classList.toggle("d-none");
+  offIcon.classList.toggle("d-none");
+
+  console.log(isMuted ? "ミュート" : "ミュート解除");
 }
